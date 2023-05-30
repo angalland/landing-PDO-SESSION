f9c9b91c7956014b0d35787667d1df721f56aeef
angalland <angalland@laposte.net>
2023-05-30 10:02:42 +0200

    ajout de commentaire db-function

--- php/db-functions.php
+++ php/db-functions.php
@@ -1,19 +1,18 @@
 <?php
 
 // Fonction connexion
-
 function connexion(){
-// connexion a la base de donnée
-try {
-    $db = new PDO (
-        'mysql:host=localhost;dbname=landing_pdo',
-        'root',
-        ''
-    );
-} catch (Execption $e) { // sinon renvoie cette erreure
-    die('Erreur :'. $e->getMessage());
-}
-return $db;
+    // connexion a la base de donnée
+    try { // essaye de se connecter
+        $db = new PDO (
+            'mysql:host=localhost;dbname=landing_pdo',
+            'root',
+            ''
+        );
+    } catch (Execption $e) { // sinon renvoie cette erreure
+        die('Erreur :'. $e->getMessage());
+    }
+    return $db;
 };
 
 // récupere toutes les données de la base de donnée
@@ -29,8 +28,6 @@ function pricing(){
     $pricings = $pricingStatment->fetchAll();
     return $pricings; // retourne le tableau
 };
-
-
 
 // recupere les donnée de la base de donnée en fonction de id saisie
 function pricingById($id){
@@ -54,6 +51,8 @@ function donneePricing($id){
    $pricings = pricingById($id); // fait appelle a la fonction idPricing avec $id donnée en argument de la fonction et la stocke dans $pricings
      
     foreach ($pricings as $pricing){ // On fait une boucle pour lire la variable pricings car la fonction pricing nous renvoie un tableau
+
+        // On assigne les données récupérées dans des variables
         $nom_pricing = $pricing['nom_pricing'];
         $price = $pricing['price'];
         $sale = $pricing['sale'];
@@ -61,8 +60,9 @@ function donneePricing($id){
         $online_space = $pricing['online_space'];
         $support = $pricing['support'];
         $domain = $pricing['domain'];
-        $hidden_fees = $pricing['hidden_fees'];?>  
+        $hidden_fees = $pricing['hidden_fees'];?>
 
+            <!-- On crée la div qui renvoie les données -->
             <div class="articlePricing">
                 <h4 class="h4Pricing">
                     <?= $nom_pricing ?> <!-- on affiche le nom pricing -->                         
@@ -81,27 +81,27 @@ function donneePricing($id){
                     <p class="p2ArticlePricing">No</p><?php
                 }
                 
-                if ($online_space != null){
-                    if ($online_space >= 1000){
-                        $online_space_unit = 'GB';
-                        $online_space = $online_space / 1000;
+                if ($online_space != null){ // si online_space existe
+                    if ($online_space >= 1000){ // détermine l'unité de online-space celon ca grandeur
+                        $online_space_unit = 'GB'; // >= 1000 -> GB
+                        $online_space = $online_space / 1000; // on divise online_space pour le faire concorder a l'unité
                     ?> 
                     <i id="iPricing" class="fa-regular fa-circle-check" style="color: #23b35a;"></i>
                     <p class="pArticlePricing">Onlinespace :</p>
                     <p class="p2ArticlePricing"><?= $online_space;?><?= $online_space_unit;?></p><?php
                     } else {
-                        $online_space_unit = 'MB';?>
+                        $online_space_unit = 'MB';?> <!-- si online_space <1000 alors unité MB -->
                         <i id="iPricing" class="fa-regular fa-circle-check" style="color: #23b35a;"></i>
                         <p class="pArticlePricing">Onlinespace :</p>
                         <p class="p2ArticlePricing"><?= $online_space;?><?= $online_space_unit;?></p><?php
                     } 
-                } else {?> <!-- sinon -->
+                } else {?> <!-- si online_space est null-->
                     <i id="iPricing" class="fa-regular fa-circle-xmark" style="color: #dd0e23;"></i>
                     <p class="pArticlePricing">Onlinespace :</p>
                     <p class="p2ArticlePricing">No</p><?php
                 }
                 
-                if ($support != null){ //si la donnée online space existe
+                if ($support != null){ //si la donnée support existe
                     if($pricing['support'] == 1){?> <!-- la donnée est vrai -->
                         <i id="iPricing" class="fa-regular fa-circle-check" style="color: #23b35a;"></i>
                         <p class="pArticlePricing">Support : </p>
@@ -114,16 +114,16 @@ function donneePricing($id){
                 }
 
                 if ($domain != null){ //<!-- si la donnée domain existe -->
-                    if ($domain < 5){?>
+                    if ($domain < 5){?> <!-- si elle inférieure a 5 -->
                     <i id="iPricing" class="fa-regular fa-circle-check" style="color: #23b35a;"></i>
                     <p class="pArticlePricing">Domain :</p>
                     <p class="p2ArticlePricing"><?= $domain;?></p><?php
-                    } else {?>
+                    } else {?> <!-- si >5 affiche unlimited -->
                         <i id="iPricing" class="fa-regular fa-circle-check" style="color: #23b35a;"></i>
                         <p class="pArticlePricing">Domain :</p>
                         <p class="p2ArticlePricing">Unlimited</p><?php  
                     }
-                } else {?> <!-- sinon -->
+                } else {?> <!-- si elle est null -->
                     <i id="iPricing" class="fa-regular fa-circle-xmark" style="color: #dd0e23;"></i>
                     <p class="pArticlePricing">Domain :</p>
                     <p class="p2ArticlePricing">No</p><?php
@@ -141,7 +141,7 @@ function donneePricing($id){
                     <p class="p2ArticlePricing">No</p><?php
                 }
 
-                if ($pricing['sale'] != null){?> <!-- si la donnée sale existe alors affiche la cette div sinon ne l'affiche pas -->                              
+                if ($pricing['sale'] != null){?> <!-- si la donnée sale existe alors affiche la div sinon ne l'affiche pas -->                              
                     <div class="divArticlePricing">
                         <p class="pdivArticlePricing"><?= $sale;?>% sale</p>
                     </div><?php
@@ -156,9 +156,12 @@ function donneePricing($id){
     }                       
 };
 
+// modifie les donnée dans la base de donnée avec la variable saisie en paramettre
 function update($data){
-
+    // se connecte a la base de donnée
     $db = connexion();
+
+    // requete sql
     $sqlQuery = 'UPDATE pricing 
                  SET
                     nom_pricing = :nom_pricing,
@@ -170,27 +173,32 @@ function update($data){
                     domain = :domain, 
                     hidden_fees = :hidden_fees
                  WHERE id_pricing = :id';
-                
+
+    //Transforme la requete sql en un objet pdo pret a etre executer 
     $updateStatment = $db->prepare($sqlQuery);
    
-        $updateStatment->bindParam("id", $data['id_pricing']);
-        $updateStatment->bindParam("nom_pricing", $data['nom_pricing']);
-        $updateStatment->bindParam("price", $data['price']);
-        $updateStatment->bindParam("sale", $data['sale']);
-        $updateStatment->bindParam("bandwitch", $data['bandwitch']);
-        $updateStatment->bindParam("online_space", $data['online_space']);
-        $updateStatment->bindParam("support", $data['support']);
-        $updateStatment->bindParam("domain", $data['domain']);
-        $updateStatment->bindParam("hidden_fees", $data['hidden_fees']);
+    // lie les parametre de la requete aux variables du tableaux $data saisie en paramettre de la fonction
+    $updateStatment->bindParam("id", $data['id_pricing']);
+    $updateStatment->bindParam("nom_pricing", $data['nom_pricing']);
+    $updateStatment->bindParam("price", $data['price']);
+    $updateStatment->bindParam("sale", $data['sale']);
+    $updateStatment->bindParam("bandwitch", $data['bandwitch']);
+    $updateStatment->bindParam("online_space", $data['online_space']);
+    $updateStatment->bindParam("support", $data['support']);
+    $updateStatment->bindParam("domain", $data['domain']);
+    $updateStatment->bindParam("hidden_fees", $data['hidden_fees']);
 
-        $updateStatment->execute();
-    };
+    // execute la requette
+    $updateStatment->execute();
+};
 
-    function formulaire($id){ 
+    //crée un formulaire en fonction de l'id dans la base de donnée pour récupéré les données saisie par l'utilisateur, pour soit modifié le formulaire, soit le supprimer.
+function formulaire($id){ 
+    // récupére les donnée en fonction de l'id
+    $pricings = pricingById($id);
 
-        $pricings = pricingById($id);
-
-        foreach ( $pricings as $pricing) {
+    // assigne ces données a des variables
+    foreach ( $pricings as $pricing) {
         $idPricing = $pricing['id_pricing'];
         $name = $pricing['nom_pricing'];
         $price = round($pricing['price'],0);
@@ -200,72 +208,90 @@ function update($data){
         $support = $pricing['support'];
         $domain = $pricing['domain'];
         $hidden_fees = $pricing['hidden_fees'];
-        }
-        
-        ?><div class="articlePricing">
-            <form action='php/traitement.php' method='post'>
+    }
+    ?>
 
-                <input type='hidden' name='id_pricing' value='<?=$idPricing?>'>
-               
-                <div>
+    <!-- crée le formulaire -->
+    <div class="articlePricing">
+        <!-- formulaire envoyé a traitement.php par post -->
+        <form action='php/traitement.php' method='post'>
+
+            <!-- récupere la valeur de l'id, non visible par l'utilisateur -->
+            <input type='hidden' name='id_pricing' value='<?=$idPricing?>'>
+
+            <!-- affiche le nom de la base de donnée et récupere le nom saisie par l'utilisateur -->
+            <div>
                 <label for='nom_pricing'>Name</label>
                 <input type='text' id='nom_pricing' name='nom_pricing' value='<?= $name;?>'>
-                </div>
-
-                <div>
+            </div>
+                
+            <!-- affiche le prix de la base de donnée et récupere le prix saisie par l'utilisateur -->
+            <div>
                 <label for='price'>Price</label>
                 <input type='number' id='price' name='price' value='<?= $price;?>'>
-                </div>
+            </div>
 
-                <div>
+            <!-- affiche le sale de la base de donnée et récupere le sale saisie par l'utilisateur -->
+            <div>
                 <label for='sale'>Sale</label>
                 <input type='number' id='sale' name='sale' min='0' max="100" step="5" value='<?= $sale;?>'>
-                </div>
+            </div>
 
-                <div>
+            <!-- affiche bandwitch de la base de donnée et récupere bandwitch saisie par l'utilisateur -->
+            <div>
                 <label for='bandwitch'>Bandwidth</label>
                 <input type='number' id='bandwitch' name='bandwitch' min='0' max='50' value='<?= $bandwitch;?>'>
-                </div>
+            </div>
 
-                <div>
+            <!-- affiche online_space de la base de donnée et récupere online_space saisie par l'utilisateur -->
+            <div>
                 <label for='online_space'>OnlineSpace</label>
                 <input  id='online_space' name='online_space' min='0' value='<?= $online_space;?>'>
-                </div>
+            </div>
 
-                <div>
+            <!-- affiche support de la base de donnée et récupere support saisie par l'utilisateur sous forme de checkbox qui renvoie 'on' si cocher et null si vide-->
+            <div>
                 <label for='support'>Support</label>
                 <input type='checkbox' name='support' <?php if ($support == 1) echo 'checked';?>>
-                </div>
+            </div>
 
-                <div>
+            <!-- affiche domain de la base de donnée et récupere domain saisie par l'utilisateur -->
+            <div>
                 <label for='domain'>Domain</label>
                 <input type='number' id='domain' name='domain' min='0' value='<?php echo $domain;?>'>
-                </div>
+            </div>
 
-                <div>
+            <!-- affiche hidden_fees de la base de donnée et récupere hidden_fees saisie par l'utilisateur sous forme de checkbox qui renvoie 'on' si cocher et null si vide-->
+            <div>
                 <label for='hidden_fees'>Hidden fees</label>
                 <input type='checkbox' name='hidden_fees' <?php if ($hidden_fees == 1) echo 'checked';?>>
-                </div><?php
+            </div>
                
-                ?>
-                <div id='divUpdate'>
+            <!-- bouton d'envoie -->
+            <div id='divUpdate'>
                 <input type='submit' name='submitUpdate' value='update' id='updateForm'>
-                </div>
-            </form>
-            <form action='php/traitement.php' method='post'>
+            </div>
+        </form>
+
+        <!-- boutton de suppression du formulaire -->
+        <form action='php/traitement.php' method='post'>
+            <!-- récupere l'id du formulaire -->
             <input type='hidden' name='id_pricing' value='<?=$idPricing?>'>
-                <div id='divUpdate'>
-                    <input type='submit' name='submitDelete' value='Delete form' id='updateForm'>
-                </div>
-            </form>           
-            </div><?php          
-        }?> 
-<?php
 
+            <div id='divUpdate'>
+                <input type='submit' name='submitDelete' value='Delete form' id='updateForm'>
+            </div>
+        </form>           
+    </div>
+<?php          
+} 
+
+// Crée une nouvelle ligne dans la table pricing dans la base de donnée
 function create($data){
-
+    // se connecte a la base de donnée
     $db = connexion();
 
+    // requetesql
     $sqlQuery = 'INSERT INTO pricing 
                 (nom_pricing, price, sale, bandwitch, online_space, support, domain, hidden_fees)
                 VALUES (:nom_pricing,
@@ -276,21 +302,25 @@ function create($data){
                         :support,
                         :domain, 
                         :hidden_fees)';
-                
-    $updateStatment = $db->prepare($sqlQuery);
-   
-        $updateStatment->bindParam("nom_pricing", $data['nom_pricing']);
-        $updateStatment->bindParam("price", $data['price']);
-        $updateStatment->bindParam("sale", $data['sale']);
-        $updateStatment->bindParam("bandwitch", $data['bandwitch']);
-        $updateStatment->bindParam("online_space", $data['online_space']);
-        $updateStatment->bindParam("support", $data['support']);
-        $updateStatment->bindParam("domain", $data['domain']);
-        $updateStatment->bindParam("hidden_fees", $data['hidden_fees']);
 
-        $updateStatment->execute();
+    //Transforme la requete sql en un objet pdo pret a etre executer    
+    $updateStatment = $db->prepare($sqlQuery);
+
+    //lie les parametres sql au valeur des variables du tableaux transmis en paramettre
+    $updateStatment->bindParam("nom_pricing", $data['nom_pricing']);
+    $updateStatment->bindParam("price", $data['price']);
+    $updateStatment->bindParam("sale", $data['sale']);
+    $updateStatment->bindParam("bandwitch", $data['bandwitch']);
+    $updateStatment->bindParam("online_space", $data['online_space']);
+    $updateStatment->bindParam("support", $data['support']);
+    $updateStatment->bindParam("domain", $data['domain']);
+    $updateStatment->bindParam("hidden_fees", $data['hidden_fees']);
+
+    // execute la requete
+    $updateStatment->execute();
 }
 
+// Crée un formulaire qui ajoutera une ligne dans la base de donnée via la fonction create()
 function divCreate(){
     ?>
 
@@ -345,6 +375,8 @@ function divCreate(){
             </div>
         </form>
     </div>
+
+    <!-- affiche un message de réussite si le formulaire est créer -->
     <div class='alert'><?php       
             if (isset($_SESSION['message'])){
                 echo $_SESSION['message'];
@@ -352,6 +384,7 @@ function divCreate(){
             };
 };
 
+// supprime une ligne dans la base de donnée
 function delete($data){
 
     $db = connexion();
